4fcb9f67228121b8b4f59ecaf0e7b2ed44fc1752
Ryan Osten <ostenrj@gmail.com>
2018-09-24 06:44:04 -0400

    improvements

--- soccer_team.py
+++ soccer_team.py
@@ -2,23 +2,32 @@ import csv
 
 if __name__ == "__main__":
 	with open("soccer_players.csv") as csvfile:
+		num_teams = 3
+
 		#read the csv store the contents of the csv in memory
 		data = csv.DictReader(csvfile, delimiter=",")
 		players = tuple(data)
 
 		#count the number of experience attributes per team
-		exp_count = {'yes': 0, 'no':0}
+		exp_count_yes = 0
 		for row in players:
 			if row['Soccer Experience'] == 'YES':
-				exp_count['yes'] += 1
-			else:
-				exp_count['no'] += 1
-		print(exp_count)
-		#determine how many of each attr to put per team
-		#split the team members into equal groups, with even experience attrs
-		#write the groups to a file
-		def createTeams(players, per_team):
-			with open("teams.txt", "a") as file:
-				file.write('some shit')
+				exp_count_yes += 1
 
-	createTeams('a', 3)
+		#identify how many experienced players to put on each team
+		exp_per_team = exp_count_yes/num_teams
+		players_per_team = len(players)/num_teams
+
+		#create a list of experienced players and non-experienced players
+
+		#assign players to each team
+
+		#write the teams to a file
+		def write_team(team, players):
+			with open("teams.txt", "a") as file:
+				file.write('\nteam\n')
+				file.write('=============')
+
+	write_team('Sharks', players)
+	write_team('Dragons', players)
+	write_team('Raptors', players)
